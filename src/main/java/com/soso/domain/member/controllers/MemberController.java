724f88e8959ae386689a5fb672d69c2caa73ec26
든든이 <wjdcofla02@naver.com>
2026-05-29 10:14:52 +0900

    gcs key git ignore설정 완

--- src/main/java/com/soso/domain/member/controllers/MemberController.java
+++ src/main/java/com/soso/domain/member/controllers/MemberController.java
@@ -1,17 +1,24 @@
 package com.soso.domain.member.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.soso.domain.member.dto.SignUpDto;
-import com.soso.domain.member.services.MemberService;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.soso.domain.member.dto.SignUpDto;
+import com.soso.domain.member.services.MemberService;
+
+import tools.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/api/member")
