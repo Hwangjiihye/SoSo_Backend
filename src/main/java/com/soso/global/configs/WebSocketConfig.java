396b94bcbfc6a7f6c210f328c55906dc42ac7cbd
황지혜 <jihye10226@naver.com>
2026-07-09 01:38:54 +0900

    웹 소켓 경로 추가

--- src/main/java/com/soso/global/configs/WebSocketConfig.java
+++ src/main/java/com/soso/global/configs/WebSocketConfig.java
@@ -22,7 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173", "https://emsemsdl.shop", "https://soso-test-1dd3d.web.app")
+                .setAllowedOriginPatterns("http://localhost:5173", "https://emsemsdl.shop", "https://soso-test-1dd3d.web.app",
+                		"https://sosoerp.shop", "https://www.sosoerp.shop")
                 .withSockJS();
     }
 }
